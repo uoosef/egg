8e1bedbf24afb202009b9a975dccb60adb9caf48
uoosef <ghobadi@yekaan.ir>
2023-06-05 18:42:11 +0330

    fix command issue

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ import (
 
 type Args struct {
 	Mode     string `short:"m" long:"mode" choice:"server" choice:"client" choice:"relay" default:"server" description:"Operational mode (server), (relay) or (client). default: server mode"`
-	Bind     string `short:"b" long:"bind" default:":8585" description:"Binding address, where should I listen to. client default: :8585, server default: :5858"`
+	Bind     string `short:"b" long:"bind" default:"127.0.0.1:8585" description:"Binding address, where should I listen to. client default: :8585, server default: :5858"`
 	Server   string `short:"s" long:"server" default:"ws://127.0.0.1:8585/ws" description:"Remote websocket server address, it should starts with ws or wss and ends with ws path ex. wss://example.com/ws"`
 	Upath    string `short:"u" long:"upload" default:"127.0.0.1:5858" description:"Separate specific path for relay ex. example.com:5858"`
-	Insecure bool   `short:"k" long:"insecure" default:"false" description:"Allow to connect to insecure end points default: false. (not recommended)"`
+	Insecure bool   `short:"k" long:"insecure" description:"Allow to connect to insecure end points default: false. (not recommended)"`
 }
 
 func main() {
@@ -21,6 +21,7 @@ func main() {
 
 	var parser = flags.NewParser(&options, flags.Default)
 	if _, err := parser.Parse(); err != nil {
+		fmt.Printf("%v", err)
 		switch flagsErr := err.(type) {
 		case flags.ErrorType:
 			if flagsErr == flags.ErrHelp {
@@ -33,16 +34,16 @@ func main() {
 	}
 	if options.Mode == "server" {
 		// run server mode, ie open http server and listen to incoming requests from internet
-		fmt.Println("Starting server at :3030 ...")
+		fmt.Printf("Starting server at %s ...", options.Bind)
 		srv := NewServer()
-		err := srv.ListenAndServe(":3030")
+		err := srv.ListenAndServe(options.Bind)
 		if errors.Is(err, http.ErrServerClosed) {
 			fmt.Printf("server closed\n")
 		} else if err != nil {
 			fmt.Printf("error starting server: %s\n", err)
 			os.Exit(1)
 		}
-	} else if options.Mode == "server" {
+	} else if options.Mode == "client" {
 		// run client mode, ie open http server and listen to incoming requests from internet
 		fmt.Println("Starting client at", options.Bind, "...")
 		srv, _ := NewClient(options.Server)
